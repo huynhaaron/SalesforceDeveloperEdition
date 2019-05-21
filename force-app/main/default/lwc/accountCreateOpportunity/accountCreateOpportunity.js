c7318e05726e0a0683fffcae68087074f5f48cea
aaron huynh <huynh.aaron@gmail.com>
2019-05-20 22:45:55 -0700

    file structure

--- force-app/main/default/lwc/accountCreateOpportunity/accountCreateOpportunity.js
+++ force-app/main/default/lwc/accountCreateOpportunity/accountCreateOpportunity.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class AccountCreateOpportunity extends LightningElement {}
+export default class AccountCreateOpportunity extends LightningElement {
+  @api recordId;
+}

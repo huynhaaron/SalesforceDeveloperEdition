decd5b1fb0a0c97abe7017bc3f1b535a9e1396fb
aaron huynh <huynh.aaron@gmail.com>
2019-05-23 10:20:20 -0700

    Created Chart JS LWC

--- force-app/main/default/lwc/lightningChart/lightningChart.js
+++ force-app/main/default/lwc/lightningChart/lightningChart.js
@@ -1,3 +1,69 @@
 import { LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+//import static resource
+import Chart from "@salesforce/resourceUrl/chart";
+//import scripts
+import { loadScript, loadStyle } from "lightning/platformResourceLoader";
 
-export default class LightningChart extends LightningElement {}
+export default class LightningChart extends LightningElement {
+  chartInitialized = false;
+
+  renderedCallback() {
+    if (this.chartInitialized) {
+      return;
+    }
+    this.chartInitialized = true;
+    Promise.all([
+      loadScript(
+        this,
+        Chart + "/chartjs-Chart.js-2166d6f/dist/Chart.bundle.js"
+      ),
+      loadStyle(this, Chart + "/chartjs-Chart.js-2166d6f/dist/Chart.css")
+    ])
+      .then(() => {
+        //console.log("Chart.JS loaded");
+        this.initializeChart();
+      })
+      .catch(error => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error loading",
+            message: error.message,
+            variant: "error"
+          })
+        );
+      });
+  }
+
+  initializeChart() {
+    var ctx = this.template.querySelector("canvas.myChart").getContext("2d");
+    this.chart = new window.Chart(ctx, {
+      // The type of chart we want to create
+      type: "line",
+
+      // The data for our dataset
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }
+        ]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+  }
+}

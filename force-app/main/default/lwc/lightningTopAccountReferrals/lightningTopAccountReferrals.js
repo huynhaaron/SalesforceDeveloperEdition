f882ed8c0ea75c927b5d8984ef8ece0212e1da20
aaron huynh <huynh.aaron@gmail.com>
2019-06-18 09:20:25 -0700

    Completed template and Chart initialization

--- force-app/main/default/lwc/lightningTopAccountReferrals/lightningTopAccountReferrals.js
+++ force-app/main/default/lwc/lightningTopAccountReferrals/lightningTopAccountReferrals.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class LightningTopAccountReferrals extends LightningElement {}
+import Chart from "@salesforce/resourceUrl/Chart";
+import { loadScript, loadStyle } from "lightning/platformResourceLoader";
+import getTopAccountReferrals from "@salesforce/apex/AccountReferralController.getTopAccountReferrals";
+
+export default class LightningTopAccountReferrals extends LightningElement {
+  chartInitialized = false;
+
+  renderedCallback() {
+    if (this.chartInitialized) {
+      return;
+    }
+    this.chartInitialized = true;
+    Promise.all([
+      loadScript(
+        this,
+        Chart + "/chartjs-Chart.js-2166d6f/dist/Chart.bundle.js"
+      ),
+      loadStyle(this, Chart + "/chartjs-Chart.js-2166d6f/dist/Chart.css")
+    ])
+      .then(() => {
+        this.initializeChart();
+      })
+      .catch(error => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error loading",
+            message: error.message,
+            variant: "error"
+          })
+        );
+      });
+  }
+
+  initializeChart() {}
+}

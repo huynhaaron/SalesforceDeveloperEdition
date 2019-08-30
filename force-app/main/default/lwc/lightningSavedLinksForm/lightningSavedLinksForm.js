d502f7b4c9f834140baf0ecfd9d0444827d3fa05
aaron huynh <huynh.aaron@gmail.com>
2019-08-30 10:57:10 -0700

    Added LWC form

--- force-app/main/default/lwc/lightningSavedLinksForm/lightningSavedLinksForm.js
+++ force-app/main/default/lwc/lightningSavedLinksForm/lightningSavedLinksForm.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import URL_FIELD from "@salesforce/schema/SavedLink__c.URL__c";
+import TITLE_FIELD from "@salesforce/schema/SavedLink__c.Title__c";
 
-export default class LightningSavedLinksForm extends LightningElement {}
+export default class LightningSavedLinksForm extends LightningElement {
+  fields = [TITLE_FIELD, URL_FIELD];
+}
